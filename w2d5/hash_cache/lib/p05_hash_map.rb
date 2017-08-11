e332d6944b64e2c948b76781e1fbcb4cfbac4e93
App Academy Student <student@appacademy.io>
2017-08-11 16:02:45 -0700

    start hashmap

--- w2d5/hash_cache/lib/p05_hash_map.rb
+++ w2d5/hash_cache/lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,27 +11,33 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    include?(key) ? bucket(key).update(key, val) : bucket(key).append(key,val)
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
   end
 
   def each
+    @store.each do |bucket_list|
+      bucket_list.each { |node| yield(node.key, node.val) }
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,9 +49,17 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { LinkedList.new }
+    @store.each do |list|
+      list.each do |node|
+        bucket(node.key).append(node.key, node.val)
+      end
+    end
+    @store = new_store
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
