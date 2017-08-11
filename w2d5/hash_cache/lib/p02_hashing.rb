51305befce8517f4114ad31685a2beb963bef0ca
App Academy Student <student@appacademy.io>
2017-08-11 14:33:24 -0700

    hash

--- w2d5/hash_cache/lib/p02_hashing.rb
+++ w2d5/hash_cache/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    return "empty array".hash if empty?
+    iDed = map.with_index { |el, i| (el.hash * (i + 1)) }
+    iDed.reduce(:+)
   end
 end
 
 class String
   def hash
+    chars.map.with_index { |ch, idx| (ch.ord * (idx + 1)).hash }.reduce(:+)
   end
 end
 
@@ -16,6 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort.hash 
   end
 end
